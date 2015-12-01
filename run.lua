a21c57e274efce184d07a0d2e60762f615afed67
Harry Rickards <rickards@mit.edu>
2015-12-01 18:59:03 +0000

    write predictions to file

--- run.lua
+++ run.lua
@@ -10,4 +10,11 @@ printf('num data = %d\n', dataset.size)
 -- load model and predict dataset
 local model = treelstm.TreeLSTMSentiment.load("trained_models/sent-constituency.2class.1l.150d.2.th")
 local predictions = model:predict_dataset(dataset)
-print(predictions)
+
+-- save predictions
+file = io.open('../sentiment/predictions.txt', 'w')
+for i = 1, predictions:size(1) do
+    file:write(predictions[i])
+    file:write("\n")
+end
+file:close()
